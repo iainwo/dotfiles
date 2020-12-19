5ccab1b20e0c0d6881fa6b790abf32d7397c11dc
Iain Wong <iainwong@outlook.com>
2020-12-19 07:47:38 +0000

    changed var path for install

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-source env/install.sh
+source ${DOTFILES_DIR:-$HOME/dotfiles}/env/install.sh
 
 echo "Installing Ubuntu packages..."
 sudo apt-get update -y
 sudo apt install -y tmux zsh git
 
-echo "Configuring Git..."
+echo "Configuring Git User: $GIT_USER with email: $GIT_EMAIL..."
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USER"
 
-echo "Installing zprezto..."
+echo "Installing zprezto into $PREZTO_INSTALL with config from $PREZTO_CONFIG..."
 export PREZTO_INSTALL="$HOME/.zprezto"
 if [ ! -d $PREZTO_INSTALL ]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git $PREZTO_INSTALL
