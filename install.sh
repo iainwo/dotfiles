5f4bc0107393de22a70ecc3beb28fdf310fa0bed
Iain Wong <iainwong@outlook.com>
2020-12-23 21:13:46 +0000

    added tree install command

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ source ${DOTFILES_DIR:-$HOME/dotfiles}/env/install.sh
 
 echo "Installing Ubuntu packages..."
 sudo apt update -y
-sudo apt install -y tmux zsh git xclip
+sudo apt install -y tmux zsh git xclip tree
 
 echo "Installing Neovim..."
 mkdir -p "$TOOLS_NVIM"
