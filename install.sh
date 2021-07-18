c8a526715acb8bf4e7921c66591fa4a6889834f5
Iain Wong <iainwong@outlook.com>
2021-07-18 13:50:07 +0000

    Added kustomize

--- install.sh
+++ install.sh
@@ -19,6 +19,11 @@ if [ -n "$INSTALL_KUBERNETES" ]; then
   curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
   chmod +x ./kubectl
   sudo mv ./kubectl /usr/local/bin/kubectl
+
+  echo "Installing kustomize..."
+  curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+  chmod +x ./kustomize
+  sudo mv ./kustomize /usr/local/bin/kustomize
 fi
 
 if [ -n "$INSTALL_DOCTL" ]; then
